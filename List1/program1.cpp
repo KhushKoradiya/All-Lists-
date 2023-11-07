6655382e9cb1600a573b14b3e6736ad5e09ebf4f
Khush <141349951+KhushKoradiya@users.noreply.github.com>
2023-11-07 07:43:07 +0530

    Create program1.cpp

---
+++ List1/program1.cpp
@@ -0,0 +1,23 @@
+/*#Write a C++ program to enter two nums and find their sum?*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int num1, num2;
+  cout<<"Koradiya Khush"<< endl;
+  cout<<"ER NO. 220130318018"<< endl;
+  
+  cout << "Enter the first number: ";
+  cin >> num1;
+
+  cout << "Enter the second number: ";
+  cin >> num2;
+
+  
+  int sum = num1 + num2;
+
+  cout << "The sum of the two numbers is " << sum << endl;
+
+  return 0;
+}
